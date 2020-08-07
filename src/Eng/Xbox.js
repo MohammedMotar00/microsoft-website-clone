a277791635e779d2f46b11f70558bcb0efa6ef85
Mohammed Motar <mohammed.motar.mm@gmail.com>
2020-08-07 20:50:23 +0200

    fixade några fel i styling

--- src/Eng/Xbox.js
+++ src/Eng/Xbox.js
@@ -6,13 +6,13 @@ import { FaAngleRight } from "react-icons/fa";
 function Xbox() {
   return (
     <section className="xbox eng">
-      <div className="content">
+      <div className="content eng">
         <h2>Xbox Game Pass Ultimate</h2>
         <p>
           Xbox Live Gold and over 100 high-quality console and PC games. Play
           together with friends and discover your next favorite game.
         </p>
-        <a href="#" className="btn">
+        <a href="#" className="btn eng">
           Join now <FaAngleRight className="angle-right" />
         </a>
       </div>
